8ac446de203422bc695a686a11484aa8e59a39b6
waveq <nynonn@gmail.com>
2015-05-25 10:58:27 +0200

    Update validate.js

--- javascript/validate.js
+++ javascript/validate.js
@@ -1,13 +1,15 @@
 $(document).ready(function (){
 
-	$("#send").click(function() {
-		validateForm();
+	$(".send").click(function() {
+		validateForm($(this));
 	});
 
-	function validateForm() {
-		$(".names").checkNames();//.addClass('gr');
-		$("#nick").regexTest({pattern: "^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]{1}[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]*$"});
-		$("#email").checkEmail();//.addClass('gr');
+
+
+	function validateForm(item) {
+		item.siblings(".names").checkNames();
+		item.siblings(".nick").regexTest({pattern: "^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]{1}[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]*$"});
+		item.siblings(".email").checkEmail();//.addClass('gr');
 	}
 		
 });
